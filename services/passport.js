b06ce6753961d5c64b2b126ae634938ec897a437
unknown <mariano_gianni@hotmail.com>
2020-07-09 19:34:08 -0300

    Added billing and client side !

--- services/passport.js
+++ services/passport.js
@@ -23,17 +23,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          console.log("User DOESN'T exists, creating record!");
-          new User({ googleId: profile.id, name: profile.displayName })
-            .save()
-            .then(savedUser => done(null, savedUser));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
