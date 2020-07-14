7b5c99851bec116bc4c317f5ce4ac10bf78aa133
unknown <mariano_gianni@hotmail.com>
2020-07-14 14:49:42 -0300

    wip

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const keys = require("./config/keys");
 const bodyParser = require("body-parser");
-require("./models/user");
+require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 moongose.connect(keys.mongoURI);
@@ -25,13 +26,14 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
