44b3713219a0ad5c48a4003d7f5c92568c290e24
Mariano Gianni <Mariano_Gianni@hotmail.com>
2020-10-28 18:08:36 -0300

    commit

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: "Survey Title",
+    name: "title",
+    noValue: "Title is required",
+  },
+  {
+    label: "Subject Line",
+    name: "subject",
+    noValue: "Email subject is required",
+  },
+  {
+    label: "Email body",
+    name: "body",
+    noValue: "Email body is required",
+  },
+  {
+    label: "Recipients list",
+    name: "recipients",
+    noValue: "A comma separated list of emails is required",
+  },
+];
